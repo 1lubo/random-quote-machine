3f37d7794fc27eddcc7840ed167d1617b8dd72da
Lubomir Duricek <1Lubo@pm.me>
2024-08-04 17:29:14 +0200

    Add function to fetch data and select quote

--- src/App.js
+++ src/App.js
@@ -1,16 +1,70 @@
 import React from 'react';
-import './App.css';
-import Button from "./components/button";
+import {random} from "lodash/number";
+import '@fontsource/roboto'
+import QuoteMachine from "./components/QuoteMachine";
+import Grid from "@mui/material/Grid";
 
-function App() {
-    function nextQuoteClickHandler() {
-        console.log('Hello World!');
+class App extends React.Component {
+    constructor(props) {
+        super(props);
+        this.state = {
+            quotes: [],
+            selectedQuoteIndex: null
+        }
+        this.assignNewQuoteIndex = this.assignNewQuoteIndex.bind(this);
+        this.generateNewQuoteIndex = this.generateNewQuoteIndex.bind(this);
     }
-  return (
-    <div className="App" id="quote-box">
-      <Button buttonDisplayName="Next Quote" clickHandler={nextQuoteClickHandler} />
-    </div>
-  );
+    componentDidMount() {
+        fetch('https://gist.githubusercontent.com/natebass/b0a548425a73bdf8ea5c618149fe1fce/raw/f4231cd5961f026264bb6bb3a6c41671b044f1f4/quotes.json')
+            .then(response => response.json())
+            .then(quotes => this.setState({ quotes: quotes }, this.assignNewQuoteIndex));
+    }
+
+    get selectedQuote() {
+        if (!this.state.quotes.length || !Number.isInteger(this.state.selectedQuoteIndex)) {
+            return undefined;
+        }
+        return this.state.quotes[this.state.selectedQuoteIndex];
+    }
+
+    /**
+     * @returns Random integer between zero and the length of the quote array.
+     * If quote array is empty, returns undefined.
+     */
+    generateNewQuoteIndex() {
+        if (!this.state.quotes.length) {
+            return undefined;
+        }
+        return random(0, this.state.quotes.length - 1);
+    }
+
+    assignNewQuoteIndex() {
+        this.setState({ selectedQuoteIndex: this.generateNewQuoteIndex() });
+    }
+
+    render() {
+        return (
+            <Grid sx={{
+                display: 'flex',
+                width: '100%',
+                height: '100vh',
+                alignItems: 'center',
+                justifyContent: 'center',
+            }}
+                  id="quote-box" container >
+                <Grid xs={3} lg={3} item>
+                    {
+                        this.selectedQuote ?
+                            <QuoteMachine selectedQuote={this.selectedQuote}
+                                          assignNewQuoteIndex={this.assignNewQuoteIndex}/> : null
+                    }
+                </Grid>
+            </Grid>
+        );
+    }
 }
+
+
+
 
 export default App;
